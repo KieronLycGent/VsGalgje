9ba8e596d314119c232416af8bf51d4d73e5105d
sepp degroote <sepp.degroote@gmail.com>
2020-11-05 12:24:55 +0100

    Nieuwe home page

--- galgje/galgje/Program.cs
+++ galgje/galgje/Program.cs
@@ -16,7 +16,7 @@ namespace galgje
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Start());
+            Application.Run(new Home());
         }
     }
 }
