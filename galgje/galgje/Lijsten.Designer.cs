a2f3b490e37dd027ef6014a1d8708ae83d990d92
sepp degroote <sepp.degroote@gmail.com>
2020-11-18 21:06:58 +0100

    begonnen aan overzicht lijsten en lijsten toevoegen

--- galgje/galgje/Lijsten.Designer.cs
+++ galgje/galgje/Lijsten.Designer.cs
@@ -29,107 +29,250 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lijsten));
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.listBox3 = new System.Windows.Forms.ListBox();
-            this.listBox4 = new System.Windows.Forms.ListBox();
-            this.listBox5 = new System.Windows.Forms.ListBox();
-            this.listBox6 = new System.Windows.Forms.ListBox();
-            this.button6 = new System.Windows.Forms.Button();
+            this.lblWhitespace = new System.Windows.Forms.Label();
+            this.btnLichaamsdelen = new System.Windows.Forms.Button();
+            this.btnCustom2 = new System.Windows.Forms.Button();
+            this.lstLichaamsdelen = new System.Windows.Forms.ListBox();
+            this.lstCustom2 = new System.Windows.Forms.ListBox();
+            this.btnGrieksAlfabet = new System.Windows.Forms.Button();
+            this.btnCustom1 = new System.Windows.Forms.Button();
+            this.btnKledij = new System.Windows.Forms.Button();
+            this.btnEten = new System.Windows.Forms.Button();
+            this.btnMeubilair = new System.Windows.Forms.Button();
+            this.btnDieren = new System.Windows.Forms.Button();
+            this.lstDieren = new System.Windows.Forms.ListBox();
+            this.lstGrieksAlfabet = new System.Windows.Forms.ListBox();
+            this.lstCustom1 = new System.Windows.Forms.ListBox();
+            this.lstKledij = new System.Windows.Forms.ListBox();
+            this.lstEten = new System.Windows.Forms.ListBox();
+            this.lstMeubilair = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lblWhitespace
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(23, 74);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(154, 244);
-            this.listBox1.TabIndex = 0;
+            this.lblWhitespace.AutoSize = true;
+            this.lblWhitespace.Location = new System.Drawing.Point(207, 469);
+            this.lblWhitespace.Name = "lblWhitespace";
+            this.lblWhitespace.Size = new System.Drawing.Size(291, 17);
+            this.lblWhitespace.TabIndex = 48;
+            this.lblWhitespace.Text = "test zodat ik het label nie opniew kwijt geraak";
             // 
-            // listBox2
+            // btnLichaamsdelen
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(183, 74);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(154, 244);
-            this.listBox2.TabIndex = 1;
+            this.btnLichaamsdelen.Enabled = false;
+            this.btnLichaamsdelen.Location = new System.Drawing.Point(174, 237);
+            this.btnLichaamsdelen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLichaamsdelen.Name = "btnLichaamsdelen";
+            this.btnLichaamsdelen.Size = new System.Drawing.Size(155, 34);
+            this.btnLichaamsdelen.TabIndex = 47;
+            this.btnLichaamsdelen.Text = "Lichaamsdelen";
+            this.btnLichaamsdelen.UseVisualStyleBackColor = true;
             // 
-            // listBox3
+            // btnCustom2
             // 
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 16;
-            this.listBox3.Location = new System.Drawing.Point(343, 74);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(154, 244);
-            this.listBox3.TabIndex = 2;
+            this.btnCustom2.Location = new System.Drawing.Point(524, 237);
+            this.btnCustom2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCustom2.Name = "btnCustom2";
+            this.btnCustom2.Size = new System.Drawing.Size(155, 34);
+            this.btnCustom2.TabIndex = 46;
+            this.btnCustom2.Text = "Custom2";
+            this.btnCustom2.UseVisualStyleBackColor = true;
             // 
-            // listBox4
+            // lstLichaamsdelen
             // 
-            this.listBox4.FormattingEnabled = true;
-            this.listBox4.ItemHeight = 16;
-            this.listBox4.Location = new System.Drawing.Point(500, 74);
-            this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(154, 244);
-            this.listBox4.TabIndex = 3;
+            this.lstLichaamsdelen.FormattingEnabled = true;
+            this.lstLichaamsdelen.ItemHeight = 16;
+            this.lstLichaamsdelen.Location = new System.Drawing.Point(174, 276);
+            this.lstLichaamsdelen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstLichaamsdelen.Name = "lstLichaamsdelen";
+            this.lstLichaamsdelen.Size = new System.Drawing.Size(153, 180);
+            this.lstLichaamsdelen.TabIndex = 45;
             // 
-            // listBox5
+            // lstCustom2
             // 
-            this.listBox5.FormattingEnabled = true;
-            this.listBox5.ItemHeight = 16;
-            this.listBox5.Location = new System.Drawing.Point(660, 74);
-            this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(154, 244);
-            this.listBox5.TabIndex = 4;
+            this.lstCustom2.FormattingEnabled = true;
+            this.lstCustom2.ItemHeight = 16;
+            this.lstCustom2.Location = new System.Drawing.Point(524, 276);
+            this.lstCustom2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstCustom2.Name = "lstCustom2";
+            this.lstCustom2.Size = new System.Drawing.Size(153, 180);
+            this.lstCustom2.TabIndex = 44;
             // 
-            // listBox6
+            // btnGrieksAlfabet
             // 
-            this.listBox6.FormattingEnabled = true;
-            this.listBox6.ItemHeight = 16;
-            this.listBox6.Location = new System.Drawing.Point(869, 74);
-            this.listBox6.Name = "listBox6";
-            this.listBox6.Size = new System.Drawing.Size(154, 244);
-            this.listBox6.TabIndex = 5;
+            this.btnGrieksAlfabet.Enabled = false;
+            this.btnGrieksAlfabet.Location = new System.Drawing.Point(334, 237);
+            this.btnGrieksAlfabet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGrieksAlfabet.Name = "btnGrieksAlfabet";
+            this.btnGrieksAlfabet.Size = new System.Drawing.Size(155, 34);
+            this.btnGrieksAlfabet.TabIndex = 43;
+            this.btnGrieksAlfabet.Text = "Grieks alfabet";
+            this.btnGrieksAlfabet.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnCustom1
             // 
-            this.button6.Location = new System.Drawing.Point(869, 30);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(154, 34);
-            this.button6.TabIndex = 11;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnCustom1.Location = new System.Drawing.Point(524, 11);
+            this.btnCustom1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCustom1.Name = "btnCustom1";
+            this.btnCustom1.Size = new System.Drawing.Size(155, 34);
+            this.btnCustom1.TabIndex = 42;
+            this.btnCustom1.Text = "Custom 1";
+            this.btnCustom1.UseVisualStyleBackColor = true;
+            // 
+            // btnKledij
+            // 
+            this.btnKledij.Enabled = false;
+            this.btnKledij.Location = new System.Drawing.Point(334, 11);
+            this.btnKledij.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnKledij.Name = "btnKledij";
+            this.btnKledij.Size = new System.Drawing.Size(155, 34);
+            this.btnKledij.TabIndex = 41;
+            this.btnKledij.Text = "Kledij";
+            this.btnKledij.UseVisualStyleBackColor = true;
+            // 
+            // btnEten
+            // 
+            this.btnEten.Enabled = false;
+            this.btnEten.Location = new System.Drawing.Point(174, 11);
+            this.btnEten.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEten.Name = "btnEten";
+            this.btnEten.Size = new System.Drawing.Size(155, 34);
+            this.btnEten.TabIndex = 40;
+            this.btnEten.Text = "Eten";
+            this.btnEten.UseVisualStyleBackColor = true;
+            // 
+            // btnMeubilair
+            // 
+            this.btnMeubilair.Enabled = false;
+            this.btnMeubilair.Location = new System.Drawing.Point(14, 11);
+            this.btnMeubilair.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMeubilair.Name = "btnMeubilair";
+            this.btnMeubilair.Size = new System.Drawing.Size(155, 34);
+            this.btnMeubilair.TabIndex = 39;
+            this.btnMeubilair.Text = "Meubilair";
+            this.btnMeubilair.UseVisualStyleBackColor = true;
+            // 
+            // btnDieren
+            // 
+            this.btnDieren.Enabled = false;
+            this.btnDieren.Location = new System.Drawing.Point(14, 237);
+            this.btnDieren.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDieren.Name = "btnDieren";
+            this.btnDieren.Size = new System.Drawing.Size(155, 34);
+            this.btnDieren.TabIndex = 38;
+            this.btnDieren.Text = "Dieren";
+            this.btnDieren.UseVisualStyleBackColor = true;
+            // 
+            // lstDieren
+            // 
+            this.lstDieren.FormattingEnabled = true;
+            this.lstDieren.ItemHeight = 16;
+            this.lstDieren.Location = new System.Drawing.Point(14, 276);
+            this.lstDieren.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstDieren.Name = "lstDieren";
+            this.lstDieren.Size = new System.Drawing.Size(153, 180);
+            this.lstDieren.TabIndex = 37;
+            // 
+            // lstGrieksAlfabet
+            // 
+            this.lstGrieksAlfabet.FormattingEnabled = true;
+            this.lstGrieksAlfabet.ItemHeight = 16;
+            this.lstGrieksAlfabet.Location = new System.Drawing.Point(334, 276);
+            this.lstGrieksAlfabet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstGrieksAlfabet.Name = "lstGrieksAlfabet";
+            this.lstGrieksAlfabet.Size = new System.Drawing.Size(153, 180);
+            this.lstGrieksAlfabet.TabIndex = 36;
+            // 
+            // lstCustom1
+            // 
+            this.lstCustom1.FormattingEnabled = true;
+            this.lstCustom1.ItemHeight = 16;
+            this.lstCustom1.Location = new System.Drawing.Point(524, 51);
+            this.lstCustom1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstCustom1.Name = "lstCustom1";
+            this.lstCustom1.Size = new System.Drawing.Size(153, 180);
+            this.lstCustom1.TabIndex = 35;
+            // 
+            // lstKledij
+            // 
+            this.lstKledij.FormattingEnabled = true;
+            this.lstKledij.ItemHeight = 16;
+            this.lstKledij.Location = new System.Drawing.Point(334, 51);
+            this.lstKledij.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstKledij.Name = "lstKledij";
+            this.lstKledij.Size = new System.Drawing.Size(153, 180);
+            this.lstKledij.TabIndex = 34;
+            // 
+            // lstEten
+            // 
+            this.lstEten.FormattingEnabled = true;
+            this.lstEten.ItemHeight = 16;
+            this.lstEten.Location = new System.Drawing.Point(174, 51);
+            this.lstEten.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstEten.Name = "lstEten";
+            this.lstEten.Size = new System.Drawing.Size(153, 180);
+            this.lstEten.TabIndex = 33;
+            // 
+            // lstMeubilair
+            // 
+            this.lstMeubilair.FormattingEnabled = true;
+            this.lstMeubilair.ItemHeight = 16;
+            this.lstMeubilair.Location = new System.Drawing.Point(14, 51);
+            this.lstMeubilair.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstMeubilair.Name = "lstMeubilair";
+            this.lstMeubilair.Size = new System.Drawing.Size(153, 180);
+            this.lstMeubilair.TabIndex = 32;
             // 
             // Lijsten
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 403);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.listBox6);
-            this.Controls.Add(this.listBox5);
-            this.Controls.Add(this.listBox4);
-            this.Controls.Add(this.listBox3);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(705, 492);
+            this.Controls.Add(this.lblWhitespace);
+            this.Controls.Add(this.btnLichaamsdelen);
+            this.Controls.Add(this.btnCustom2);
+            this.Controls.Add(this.lstLichaamsdelen);
+            this.Controls.Add(this.lstCustom2);
+            this.Controls.Add(this.btnGrieksAlfabet);
+            this.Controls.Add(this.btnCustom1);
+            this.Controls.Add(this.btnKledij);
+            this.Controls.Add(this.btnEten);
+            this.Controls.Add(this.btnMeubilair);
+            this.Controls.Add(this.btnDieren);
+            this.Controls.Add(this.lstDieren);
+            this.Controls.Add(this.lstGrieksAlfabet);
+            this.Controls.Add(this.lstCustom1);
+            this.Controls.Add(this.lstKledij);
+            this.Controls.Add(this.lstEten);
+            this.Controls.Add(this.lstMeubilair);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Lijsten";
             this.Text = "Lijsten";
             this.Load += new System.EventHandler(this.Lijsten_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.ListBox listBox3;
-        private System.Windows.Forms.ListBox listBox4;
-        private System.Windows.Forms.ListBox listBox5;
-        private System.Windows.Forms.ListBox listBox6;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label lblWhitespace;
+        private System.Windows.Forms.Button btnLichaamsdelen;
+        private System.Windows.Forms.Button btnCustom2;
+        private System.Windows.Forms.ListBox lstLichaamsdelen;
+        private System.Windows.Forms.ListBox lstCustom2;
+        private System.Windows.Forms.Button btnGrieksAlfabet;
+        private System.Windows.Forms.Button btnCustom1;
+        private System.Windows.Forms.Button btnKledij;
+        private System.Windows.Forms.Button btnEten;
+        private System.Windows.Forms.Button btnMeubilair;
+        private System.Windows.Forms.Button btnDieren;
+        private System.Windows.Forms.ListBox lstDieren;
+        private System.Windows.Forms.ListBox lstGrieksAlfabet;
+        private System.Windows.Forms.ListBox lstCustom1;
+        private System.Windows.Forms.ListBox lstKledij;
+        private System.Windows.Forms.ListBox lstEten;
+        private System.Windows.Forms.ListBox lstMeubilair;
     }
 }
